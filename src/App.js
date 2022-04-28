7ab1426dd62d220fb0be9ac1a6cb691d2f4cec18
Fiston <60503084+Fiston-pro@users.noreply.github.com>
2022-04-28 13:29:30 +0200

    I did the welcoming page and installed axios for making api calls

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import './App.css';
+import Axios from "axios";
+import { useState } from "react";
 
 function App() {
+  const [number, setnumber] = useState();
+
+  const getData = async () => {
+    await Axios.get('https://random-words-api.vercel.app/word').then(res => {
+      console.log(res.data)
+    })
+  };
+
+  // We avoid reloading the whole screen by using this function
+  const onSubmit = (e) => {
+    e.preventDefault();
+    getData();
+  };
+
   return (
     <div className="App">
-      <h1>Please enter a number btn  5 and 20</h1>
+      <h1 className='header'>Please enter a number btn  5 and 20</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          className="app__input"
+          type="text"
+          placeholder="A number"
+          autoComplete="Off"
+          value={number}
+          onChange={(e) => setnumber(e.target.value)}
+        />
+        <input className="app__submit" type="submit" value="Search" />
+      </form>
     </div>
   );
 }
