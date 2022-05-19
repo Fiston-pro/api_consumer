c2f3ad6cfe9e736b0bca1f9f1f68b8557595f61e
Fiston <60503084+Fiston-pro@users.noreply.github.com>
2022-05-19 23:58:29 +0200

    Update index.jsx

--- src/component/tile/index.jsx
+++ src/component/tile/index.jsx
@@ -3,7 +3,7 @@ import "./style.css";
 
 export default function WordTile({ data }) {
   return (
-      <div className="wordTile">
+      <div className="wordTile"> //data to display in this component
           <p className="wordTile__name">Word: {data.word}</p> 
           <p className="wordTile__name">Definition: {data.definition}</p>
           <p className="wordTile__name">Pronunciation: {data.pronunciation}</p>
